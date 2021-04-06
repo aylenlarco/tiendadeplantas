c477105680b8d2555a7b7f7a6dbbbd55d94ea940
repositorio_morganconsulting <aylenlarco@gmail.com>
2021-04-06 05:04:24 -0300

    first commit

--- js/cart.js
+++ js/cart.js
@@ -23,7 +23,7 @@
                 
                     modalCart( plantImg, plantTitle, plantPrice );
 
-                    cartCounterUpdate()
+                    cartCounterUpdate();
                     
                 };    
                
